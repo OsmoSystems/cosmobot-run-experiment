29a23a618d3a543f7f3a18fae6f52f7419a60dc5
Unknown <jeremy@dubchuck.com>
2018-11-08 14:11:03 -0800

    Add test for additional_sync_params

--- cosmobot_run_experiment/s3_test.py
+++ cosmobot_run_experiment/s3_test.py
@@ -20,3 +20,13 @@ class TestSyncToS3:
 
         expected_command = f'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
         mock_check_call.assert_called_with(expected_command, shell=True)
+
+    def test_syncs_to_subdirectory_in_s3_bucket_with_additional_sync_params(self, mocker, mock_check_call):
+        mock_path_basename = mocker.patch('os.path.basename')
+        mock_path_basename.return_value = 'experiment_name'
+
+        module.sync_to_s3(local_sync_dir='/output_dir/experiment_name', additional_sync_params='--exclude *.log*')
+
+        expected_command = f'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name ' \
+                           '--exclude *.log*'
+        mock_check_call.assert_called_with(expected_command, shell=True)
