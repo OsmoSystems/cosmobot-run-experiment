829fe79714a07c38be33eb5a165e45b92cc88867
Unknown <jeremy@dubchuck.com>
2018-12-05 06:14:52 -0800

    Mock killing children

--- cosmobot_run_experiment/sync_manager_test.py
+++ cosmobot_run_experiment/sync_manager_test.py
@@ -14,7 +14,9 @@ def mock_sync_process(mocker):
 def mock_psutil_process(mocker):
     mock_psutil_process = mocker.patch.object(psutil, 'Process')
     mock_psutil_process.return_value.pid = 10000
-    mock_psutil_process.return_value.children = lambda recursive: []
+    mock_psutil_process.return_value.children.return_value = [
+        type('obj', (object,), { 'kill' : lambda: True } )
+    ]
     mock_psutil_process.return_value.kill.return_value = True
     return mock_psutil_process
 
@@ -65,3 +67,4 @@ class TestEndSyncingProcess:
         mocker.patch.object(module, '_is_sync_process_running').return_value = True
         module.end_syncing_process()
         assert mock_psutil_process.return_value.kill.call_count == 1
+        assert mock_psutil_process.return_value.children[0]['kill'].call_count == 1
