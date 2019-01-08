d40e4c19d3cbc8248fd919ad2595cd693c4f9dc3
Unknown <jeremy@dubchuck.com>
2019-01-08 12:52:13 -0800

    color channel overexposure percent, update tests

--- cosmobot_run_experiment/exposure_test.py
+++ cosmobot_run_experiment/exposure_test.py
@@ -22,3 +22,13 @@ class TestGenerateStastics():
         actual_underexposed_percent = stats['underexposed_percent']
         expected_underexposed_percent = 0.25
         assert actual_underexposed_percent == expected_underexposed_percent
+
+    @pytest.mark.parametrize("name, color_channel_index, expected_color_channel_overexposed_percent", [
+        ('R channel', "overexposed_percent_r", 0.25),
+        ('G channel', "overexposed_percent_g", 0.25),
+        ('B channel', "overexposed_percent_b", 0.25)
+    ])
+    def test_underexposed_percent_by_color_channel(self, name, color_channel_index, expected_color_channel_overexposed_percent):
+        stats = module._generate_statistics(rgb_image)
+        actual_color_channel_overexposed_percent = stats[color_channel_index]
+        assert actual_color_channel_overexposed_percent == expected_color_channel_overexposed_percent
