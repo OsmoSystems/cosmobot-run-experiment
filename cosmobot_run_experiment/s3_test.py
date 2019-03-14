cc3c6de570055e868d00271a74e06358eecc2261
Unknown <jeremy@dubchuck.com>
2019-03-14 10:16:28 -0700

    Update s3 test to include new sudo usage

--- cosmobot_run_experiment/s3_test.py
+++ cosmobot_run_experiment/s3_test.py
@@ -22,7 +22,7 @@ class TestSyncToS3:
     def test_syncs_to_subdirectory_in_s3_bucket(self, mocker, mock_check_call, mock_path_basename):
         module.sync_to_s3(local_sync_dir='/output_dir/experiment_name')
 
-        expected_command = 'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
+        expected_command = 'sudo AWS_SHARED_CREDENTIALS_FILE=/home/pi/.aws/credentials /home/pi/.local/bin/aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
         mock_check_call.assert_called_with(expected_command, shell=True)
 
     def test_syncs_to_subdirectory_in_s3_bucket_with_additional_sync_params(
@@ -30,7 +30,7 @@ class TestSyncToS3:
     ):
         module.sync_to_s3(local_sync_dir='/output_dir/experiment_name', additional_sync_params='--exclude *.log*')
 
-        expected_command = 'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name ' \
+        expected_command = 'sudo AWS_SHARED_CREDENTIALS_FILE=/home/pi/.aws/credentials /home/pi/.local/bin/aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name ' \
                            '--exclude *.log*'
         mock_check_call.assert_called_with(expected_command, shell=True)
 
@@ -38,9 +38,13 @@ class TestSyncToS3:
         (
             'mv',
             True,
-            'aws s3 mv --recursive /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
+            'sudo AWS_SHARED_CREDENTIALS_FILE=/home/pi/.aws/credentials /home/pi/.local/bin/aws s3 mv --recursive /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
         ),
-        ('sync', False, 'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name ')
+        (
+            'sync',
+            False,
+            'sudo AWS_SHARED_CREDENTIALS_FILE=/home/pi/.aws/credentials /home/pi/.local/bin/aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name '
+        )
     ])
     def test_s3_subcommand(
         self, test_name, erase_synced_files, expected_command, mocker, mock_check_call, mock_path_basename
