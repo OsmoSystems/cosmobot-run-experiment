196b6c3613efd5c9e5442ddff0a9fb979eb5c35d
thatneat <thatneat@users.noreply.github.com>
2019-04-08 16:49:07 -0700

    Add variant params to filename (#23)
    * Add variant params to filename
    
    * lint
    
    * remove unused variable
    
    * minor PR fixes

--- cosmobot_run_experiment/file_structure.py
+++ cosmobot_run_experiment/file_structure.py
@@ -6,11 +6,46 @@ def get_base_output_path():
     return '/home/pi/camera-sensor-output/'
 
 
-def iso_datetime_for_filename(datetime):
+def iso_datetime_for_filename(datetime_):
     ''' Returns datetime as a ISO-ish format string that can be used in filenames (which can't inclue ":")
         datetime(2018, 1, 1, 12, 1, 1) --> '2018-01-01--12-01-01'
     '''
-    return datetime.strftime('%Y-%m-%d--%H-%M-%S')
+    return datetime_.strftime('%Y-%m-%d--%H-%M-%S')
+
+
+def _process_param_for_filename(param):
+    ''' prep param for a filename by removing common problem characters
+    Args:
+        param: parameter value - expected to be a string or number
+    Returns:
+        string that is safer to use as a filename
+    '''
+    handled_types = (str, int, float)
+    if not isinstance(param, handled_types):
+        raise TypeError(
+            'I\'m not sure I know how to handle this param of type {type}: {param}'.format(
+                type=type(param), param=param
+            )
+        )
+    return str(param).replace('-', '').replace(' ', '_')
+
+
+def get_image_filename(current_datetime, variant):
+    '''
+    Args:
+        current_datetime: datetime.datetime instance for when the image is taken
+        variant: ExperimentVariant instance for this image
+
+    Returns:
+        string - image filename including extension
+    '''
+    iso_ish_datetime = iso_datetime_for_filename(current_datetime)
+    variant_params_for_filename = '_'.join(
+        '{}_{}'.format(_process_param_for_filename(key), _process_param_for_filename(value))
+        for key, value in variant._asdict().items()
+    )
+    image_filename = '{iso_ish_datetime}_{variant_params_for_filename}_.jpeg'.format(**locals())
+    return image_filename
 
 
 def get_files_with_extension(directory, extension):
--- cosmobot_run_experiment/file_structure_test.py
+++ cosmobot_run_experiment/file_structure_test.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+import pytest
+
+from cosmobot_run_experiment.prepare import ExperimentVariant
 from . import file_structure as module
 
 
@@ -9,3 +12,51 @@ class TestIsoDatetimeForFilename:
         expected = '2018-01-02--13-14-15'
 
         assert actual == expected
+
+
+class TestProcessParamStringsForFilename:
+    def test_replaces_spaces(self):
+        assert module._process_param_for_filename('test this thing') == 'test_this_thing'
+
+    def test_removes_dashes(self):
+        assert module._process_param_for_filename('test-this-thing') == 'testthisthing'
+
+    def test_handles_float(self):
+        assert module._process_param_for_filename(0.0) == '0.0'
+
+    def test_handles_int(self):
+        assert module._process_param_for_filename(0) == '0'
+
+    def test_blows_up_with_unhandled_type(self):
+        with pytest.raises(TypeError):
+            module._process_param_for_filename({'I am': 'a dictionary!!!'})
+
+
+class TestGetImageFilename:
+    example_variant = ExperimentVariant(
+        capture_params=' -ss 1234 -ISO 5678',
+        led_warm_up=0.1,
+        led_color='red',
+        led_intensity=1.0,
+        use_one_led=True,
+        led_cool_down=99,
+    )
+    datetime_ = datetime(2019, 4, 8, 9, 52, 12)
+
+    def test_get_image_filename_includes_datetime(self):
+        iso_ish_datetime = module.iso_datetime_for_filename(self.datetime_)
+        assert iso_ish_datetime in module.get_image_filename(self.datetime_, self.example_variant)
+
+    def test_get_image_filename_includes_capture_params_with_spaces_replaced(self):
+        assert '_ss_1234_ISO_5678_' in module.get_image_filename(self.datetime_, self.example_variant)
+
+    def test_get_image_filename_includes_led_capture_params(self):
+        expected_led_params_string = (
+            'led_warm_up_0.1_led_color_red_led_intensity_1.0_use_one_led_True_led_cool_down_99'
+        )
+        assert expected_led_params_string in module.get_image_filename(self.datetime_, self.example_variant)
+
+    def test_get_image_filename_ends_with_underscore(self):
+        # This is so that if certain capture params are missing, you can still grep in filenames for a particular
+        # capture param -- eg. you can grep for "iso_100_" and have it not match iso_1000 variants as well.
+        assert module.get_image_filename(self.datetime_, self.example_variant).endswith('_.jpeg')
