8f8ccb3791c7f53d1abd502a14a6f5dbcc96a283
thatneat <thatneat@gmail.com>
2019-08-22 14:50:19 -0700

    remove timing check

--- cosmobot_run_experiment/led_control.py
+++ cosmobot_run_experiment/led_control.py
@@ -19,10 +19,9 @@ DIGITAL_LED_PIN_ON_LOW = board.D6
 
 
 def _set_dio_pin(pin, value: bool):
-    now = datetime.datetime.now().isoformat()
     logging.info(
-        "Setting DIO pin (T={time}) {pin} -> {pin_setpoint}".format(
-            time=now, pin=pin, pin_setpoint="high" if value else "low"
+        "Setting DIO pin {pin} -> {pin_setpoint}".format(
+            pin=pin, pin_setpoint="high" if value else "low"
         )
     )
 
